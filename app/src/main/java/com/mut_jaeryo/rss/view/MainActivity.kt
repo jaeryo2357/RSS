3f10872bef066b079fa53a242c518c5c07ea32dc
jaeryo2357 <jaeryo2357@naver.com>
2020-03-24 17:43:10 +0900

    다시 정리

--- app/src/main/java/com/mut_jaeryo/rss/view/MainActivity.kt
+++ app/src/main/java/com/mut_jaeryo/rss/view/MainActivity.kt
@@ -2,17 +2,11 @@ package com.mut_jaeryo.rss.view
 
 import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.net.NetworkInfo
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.mut_jaeryo.rss.R
@@ -20,6 +14,7 @@ import com.mut_jaeryo.rss.data.RssData
 import com.mut_jaeryo.rss.data.RssViewModel
 import com.mut_jaeryo.rss.databinding.ActivityMainBinding
 import com.mut_jaeryo.rss.util.RssAdapter
+import com.mut_jaeryo.rss.util.isInternetAvailable
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
@@ -50,7 +45,6 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
             viewModel.loadRssToUrl(applicationContext.getString(R.string.rss_url))
         else
             Toast.makeText(this@MainActivity,getString(R.string.internet_error),Toast.LENGTH_LONG).show()
-
         main_swipe_layout.setOnRefreshListener(this)
     }
 
@@ -60,35 +54,5 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
         else
             viewModel.onRssRefresh()
         main_swipe_layout.isRefreshing = false
-    }
-
-    fun isInternetAvailable(context:Context):Boolean{
-        var result = false
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val networkCapabilities = connectivityManager.activeNetwork ?: return false
-            val actNw =
-                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
-            result = when {
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
-        } else {
-            connectivityManager.run {
-                connectivityManager.activeNetworkInfo?.run {
-                    result = when (type) {
-                        ConnectivityManager.TYPE_WIFI -> true
-                        ConnectivityManager.TYPE_MOBILE -> true
-                        ConnectivityManager.TYPE_ETHERNET -> true
-                        else -> false
-                    }
-
-                }
-            }
-        }
-        return result
     }
 }
